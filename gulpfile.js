45aeba693572746488bff4b6f4edf580304319bc
Abialbon Paul <dr.abialbon@gmail.com>
2017-07-30 13:07:44 -0400

    Add SCSS workflow

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const scss = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+gulp.task('scss', () => {
+   return gulp.src('app/scss/**/*.scss')
+       .pipe(scss())
+       .pipe(gulp.dest('app/css'))
+       .pipe(browserSync.stream())
+});
+
+gulp.task('browsersync', () => {
+   browserSync.init({
+       server: {
+           baseDir: 'app'
+       }
+   });
+});
+
+gulp.task('serve', ['browsersync'], () => {
+   gulp.watch('app/scss/**/*.scss', ['scss']);
+   gulp.watch('app/*.html', browserSync.reload);
+});
